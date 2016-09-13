f064dffa488252e543b8af06a5331219ca7a64a3
LambdaXymox <lambda.xymox@gmail.com>
2016-09-12 21:57:47 -0500

    Add BF instruction set.

---
+++ src/Brainfuck.hs
@@ -0,0 +1,27 @@
+module Brainfuck where
+
+
+data BFProg = BFProg [BFInstruction]
+
+data BFInstruction = BFPointerInc
+                   | BFPointerDec
+                   | BFPointerDerefInc
+                   | BFPointerDerefDec
+                   | BFReadByte
+                   | BFWriteByte
+                   | BFBeginLoop
+                   | BFEndLoop
+                   | BFLoop [BFInstruction]
+
+instance Show BFProg where
+    show BFProg = concatMap . show
+
+instance Show BFInstruction where
+    show BFPointerInc      = ">"
+    show BFPointerDec      = "<"
+    show BFPointerDerefInc = "+"
+    show BFPointerDerefDec = "-"
+    show BFReadByte        = ","
+    show BFWriteByte       = "."
+    show BFBeginLoop       = "["
+    show BFEndLoop         = "]"
